bc7eada812bc7f2c3fa56abbd62b96a6b39bdedb
Fritz Ammon <fritz.ammon@slickdeals.net>
2020-10-18 16:44:32 -0700

    testing how placement of modifiers changes the effect e.g. padding at start vs end

--- swiftui/basics.playground/Contents.swift
+++ swiftui/basics.playground/Contents.swift
@@ -6,8 +6,16 @@ import PlaygroundSupport
 struct BasicView: View {
     var body: some View {
         VStack {
-            Text("Hello World")
+            Text("Hello World (w/ padding at start)")
                 .padding()
+                .foregroundColor(.white)
+                .background(Color.black)
+            Text("Hello World v2 (w/ padding at end)")
+                .foregroundColor(.white)
+                .background(Color.black)
+                .padding()
+                .background(Color.blue)
+            Text("Hello World v3 (w/o padding)")
                 .foregroundColor(.white)
                 .background(Color.black)
         }
